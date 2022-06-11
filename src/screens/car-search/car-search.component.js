3fbaaab5f08faa59a05cf1063706d4eb8c3a6311
Olie440 <accounts+github@oliverrushworth.uk>
2022-06-11 13:55:37 +0000

    test commit

--- src/screens/car-search/car-search.component.js
+++ src/screens/car-search/car-search.component.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import SearchResults from './components/search-results/search-results.component';
-import './car-search.style.css';
+import React from "react";
+import SearchResults from "./components/search-results/search-results.component";
+import "./car-search.style.css";
 
 export default function CarSearchScreen() {
-    return (
-        <div className="car-search screen background--blue">
-            <div className="car-search__search-container background--yellow">
-                <h2 className="car-search__header">
-                    Let’s find your ideal car
-                </h2>
-                <label className="car-search__label" htmlFor="car-search__search">
-                    Pick-up Location
-                </label>
-                <SearchResults />
-            </div>
-        </div>
-    );
+  return (
+    <div className="car-search screen background--blue">
+      <div className="car-search__search-container background--yellow">
+        <h2 className="car-search__header">Let’s find your ideal car</h2>
+        <label className="car-search__label" htmlFor="car-search__search">
+          Pick-up Location test
+        </label>
+        <SearchResults />
+      </div>
+    </div>
+  );
 }
